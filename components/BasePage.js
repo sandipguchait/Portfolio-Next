6b0de8e656fe3c3fad2002adc5aff3cf3902189a
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-05-09 14:17:55 +0530

    custom component to show header and props.children

---
+++ components/BasePage.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import { Container } from 'reactstrap';
+
+
+const BasePage = (props) => {
+  return (
+    <div className="base-page">
+      <Container>
+        {props.children}
+      </Container>
+    </div>
+  );
+};
+
+export default BasePage;
