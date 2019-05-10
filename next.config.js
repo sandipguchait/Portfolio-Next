636a0ca1da7705328f93cb95fbb90d078274667d
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-05-10 15:52:46 +0530

    fixing bugs and deploying to now.sh

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
-// next.config.js
-const withSass = require('@zeit/next-sass')
-const withCSS = require('@zeit/next-css')
-module.exports = withCSS(withSass());
+module.exports = {
+  target: 'serverless'
+}
+
+const { PHASE_PRODUCTION_SERVER } =
+  process.env.NODE_ENV === 'development'
+    ? {}
+    : !process.env.NOW_REGION
+      ? require('next/constants')
+      : require('next-server/constants');
+
+module.exports = (phase, { defaultConfig }) => {
+  if (phase === PHASE_PRODUCTION_SERVER) {
+    // Config used to run in production.
+    return {};
+  }
+
+  // ✅ Put the require call here.
+  const withCSS = require('@zeit/next-css');
+  const withSass = require('@zeit/next-sass')
+
+  return withCSS(withSass());
+};
