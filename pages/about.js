68e15f8b72ab623e13ac6cf834bd394355aae103
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-05-10 14:02:10 +0530

    completing cv page

--- pages/about.js
+++ pages/about.js
@@ -13,24 +13,23 @@ class About extends Component {
         <Row className="mt-5">
             <Col md="6">
               <div className="left-side">
-                <h1 className="title fadein">Hello, Welcome</h1>
-                <h4 className="subtitle fadein">To About Page</h4>
+                <h1 className="title fadein">Hello, There</h1>
+                <h4 className="subtitle fadein">To My Bio</h4>
                 <p className="subsubTitle fadein">Feel free to read short description about me.</p>
               </div>
             </Col>
             <Col md="6">
               <div className="fadein" id="intro">
-                <p>My name is Filip Jerga and I am an experienced software engineer and freelance developer. </p>
+                <p>My name is Sandip Guchait and I am an Self-taught Web Developer. </p>
                 <p>
-                I have a Master's degree in Artificial Intelligence and several years of experience working
-                on a wide range of technologies and projects from C++ development for ultrasound devices to
+                I have a Bachelor's degree in Computer Science and experience on working
+                on a wide range of technologies and projects from AWS to GraphQL to make
                 modern mobile and web applications in React and Angular.
                 </p>
                 <p>
                 Throughout my career, I have acquired advanced technical knowledge and the ability to explain
-                programming topics clearly and in detail to a broad audience. I invite you to take my course,
-                where I have put a lot of effort to explain web and software engineering concepts in a detailed,
-                hands-on and understandable way.
+                programming topics clearly and in detail to a broad audience. I love to Learn from Experienced
+                Developers and yes I am a great Team Player.
                 </p>
               </div>
             </Col>
--- pages/cv.js
+++ pages/cv.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import BaseLayout from '../components/layouts/BaseLayout';
 import BasePage from '../components/BasePage';
+import { Row, Col } from 'reactstrap';
+
 
 class Cv extends Component {
 
   render() {
     return (
       <BaseLayout>
-        <BasePage>
-          Cv
+        <BasePage title="Read My CV" className="cv-page">
+        <Row>
+          <Col md={{ size: 8, offset: 2}}>
+            <div className="cv-title">
+              <a download="Sandip.pdf" className="btn btn-success" href="/static/images/Sandip.pdf">
+              Download
+              </a>
+            </div>
+            <img style={{ width: '100%', height: '800px'}} src="/static/images/sandip.jpg"/>
+          </Col>
+        </Row>
         </BasePage>
       </BaseLayout>
     );
